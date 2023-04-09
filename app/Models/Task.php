9af3cc7bf60d03961fc8441f9f5a72aa07cfcd28
keymiro <44626685+keymiro@users.noreply.github.com>
2023-04-09 18:19:25 -0500

    added attributes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,6 +9,19 @@ class Task extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'start',
+        'end',
+        'user_id',
+    ];
+
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
     }
